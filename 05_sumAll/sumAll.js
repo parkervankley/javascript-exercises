e63c8f5d4b2947cdd13feaba05ac40ce71a5cba6
parkervankley <parkerjvankley@gmail.com>
2024-10-11 09:56:29 -0500

    05_sumAll complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(intOne, intTwo) {
+  if (intOne < 0 || intTwo < 0 || !Number.isInteger(intOne) || !Number.isInteger(intTwo)) return "ERROR";
+  let sum = 0;
+  for (i = Math.min(intOne, intTwo); i <= Math.max(intOne, intTwo); i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
