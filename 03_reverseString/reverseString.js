e5dbdc7d965975032ce626243ed077629027ce0e
parkervankley <parkerjvankley@gmail.com>
2024-10-10 20:09:17 -0500

    03_reverseString complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+  const stringArr = stringToReverse.split("");
+  const reverseStringArr = [];
+  for(const letter of stringArr) {
+    reverseStringArr.unshift(letter);
+  }
+  return reverseStringArr.join("");
 };
 
 // Do not edit below this line
