2950077ef9d3a6611136e46325bfccbf371a9e97
parkervankley <parkerjvankley@gmail.com>
2024-10-11 10:22:08 -0500

    07_tempConversion complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conversion = ((temp - 32) * (5/9));
+  if (conversion % 1 != 0) return Number(conversion.toFixed(1));
+  return conversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let conversion = (temp * (9/5) + 32);
+  if (conversion % 1 != 0) return Number(conversion.toFixed(1));
+  return conversion;
 };
 
 // Do not edit below this line
