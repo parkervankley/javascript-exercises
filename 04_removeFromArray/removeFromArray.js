b8d957ee36fef0e2e3af208fae8f0d8a3d4bd37e
parkervankley <parkerjvankley@gmail.com>
2024-10-11 09:45:12 -0500

    04_removeFromArray complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementsToRemove) {
+  for (element of elementsToRemove) {
+    arr = arr.filter(item => item !== element);
+  }
+  return arr;
 };
 
 // Do not edit below this line
