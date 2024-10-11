141cd35dab115e0d357ec2e8d14a4a924967ee79
parkervankley <parkerjvankley@gmail.com>
2024-10-10 19:56:57 -0500

    02_repeatString complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, numIterations) {
+  let returnText = "";
+  if(numIterations < 0) return "ERROR";
+  for (let i = 0; i < numIterations; i++) {
+    returnText += word;
+  }
+  return returnText;
 };
 
 // Do not edit below this line
