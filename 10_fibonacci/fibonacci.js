4e81d4e6f60495a1cb2044ddf5f93214fc3aa952
parkervankley <parkerjvankley@gmail.com>
2024-10-23 21:11:22 -0500

    10_fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+  a = parseInt(a);
+  if (a < 0) return "OOPS";
+  if (a === 0) return 0;
+  if (a === 1) return 1;
+  return fibonacci(a - 1) + fibonacci(a - 2);
 };
 
 // Do not edit below this line
