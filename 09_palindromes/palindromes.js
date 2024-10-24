034a959ea30f774e20ca2163f6dc3facdcb44219
parkervankley <parkerjvankley@gmail.com>
2024-10-23 20:54:25 -0500

    09_palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  word = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  let reversed = [...word].reverse().join('');
+  console.log(reversed);
+  return word === reversed;
 };
 
 // Do not edit below this line
